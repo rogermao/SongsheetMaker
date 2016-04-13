97cbfec9317d21863496c09cc90a2afcc991484d
Sunny Duan <sunnyddelight1@gmail.com>
2016-04-13 01:10:02 -0500

    Parser

--- parseXml.py
+++ parseXml.py
@@ -1,30 +1,47 @@
+from collections import defaultdict
+import re
+
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('Songbook.xml')
+def check_chords(line):
+    if len(line) < 1:
+        return False
+    wordsplit = re.findall(r"[\w]+", line)
+    if len(wordsplit) < 1:
+        return False
+    elif max([len(word) for word in wordsplit]) < 4 or any([bool(re.search(r"[A-Z]sus", word)) for word in wordsplit]):
+        return False
+    else:
+        return True
+def main():
+    tree = ET.parse('Songbook.xml')
 
-root = tree.getroot()
+    root = tree.getroot()
 
-pages = root.findall('page')
-pages = pages[4:]
+    pages = root.findall('page')
+    pages = pages[4:]
 
-songs = {}
+    songs = defaultdict(list)
 
-for page in pages:
-    song = {}
-    currentSong = ""
-    verseCount = 0
-    emptyLine = False
-    for text in pages.findall('text'):
-        #Then song title
-        if text.find('b') is not None:
-            songs[currentSong] = song
-            song = {}
-            currentSong = text.find('b').text
-            verseCount = 0
-            emptyLine = True
-        #Then 
-        elif text.text is None:
-            if emptyLine:
-                
-            else:
+    for page in pages:
+        currentSong = "contents"
+        verseCount = 0
+        emptyLine = False
+        for text in page.findall('text'):
+            #Then song title
+            if text.find('b') is not None:
+                currentSong = text.find('b').text
+                verseCount = 0
                 emptyLine = True
+            #Then
+            else:
+                if text.text != None and check_chords(text.text):
+                    songs[currentSong].append(text.text)
+    for song in songs:
+        print song
+        for verse in songs[song]:
+            print verse
+        print "\n\n"
+
+if __name__ == '__main__':
+    main()
